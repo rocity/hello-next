7ff2685e8d2406db48d080a350e4b5b7c653ebc6
Kevin Kalis <kevinpaulkalis@gmail.com>
2019-05-28 14:41:06 +0800

    Add detail page

--- comps/Layout.js
+++ comps/Layout.js
@@ -8,7 +8,6 @@ const layoutStyle = {
 
 const Layout = props => (
   <div style={layoutStyle}>
-    <h1>LAYOUT</h1>
     <Header />
     {props.children}
   </div>
--- pages/Index.js
+++ pages/Index.js
@@ -1,8 +1,23 @@
 import Layout from '../comps/Layout';
+import Link from 'next/link';
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
 const Index = () => (
   <Layout>
-    <p>Hello Next.js</p>
+    <h1>Blog</h1>
+
+    <ul>
+      <PostLink title="Hello summit!"></PostLink>
+      <PostLink title="Hello C2!"></PostLink>
+      <PostLink title="Hello Cricket!"></PostLink>
+    </ul>
   </Layout>
 )
 
